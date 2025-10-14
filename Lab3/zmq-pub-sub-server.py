883b19b17de002590be8becc2036d5f3f4b92f1c
gamba9911 <237609664+gamba9911@users.noreply.github.com>
2025-10-14 18:08:46 +0200

    Lab 3 - Task 4 - ZMQ Publish-Subscribe

---
+++ Lab3/zmq-pub-sub-server.py
@@ -0,0 +1,16 @@
+# Weather update server
+# Binds PUB socket to tcp://*:5556
+# Publishes random weather updates
+from random import randrange
+
+import zmq
+
+context = zmq.Context()
+
+socket = context.socket(zmq.PUB)
+socket.bind("tcp://localhost:5556")
+while True:
+    zipcode = randrange(1, 100000)
+    temperature = randrange(-80, 135)
+    relhumidity = randrange(10, 60)
+    socket.send_string("%i %i %i" % (zipcode, temperature, relhumidity))
