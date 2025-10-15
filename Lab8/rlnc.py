8366010c087a869b9204ee72b4e44ac2f9f7481e
gamba9911 <237609664+gamba9911@users.noreply.github.com>
2025-10-15 23:50:21 +0200

    Lab 8 - week starter files

---
+++ Lab8/rlnc.py
@@ -0,0 +1,23 @@
+import pyerasure
+import pyerasure.finite_field
+import pyerasure.generator
+import math
+import random
+import copy # for deepcopy
+from utils import random_string
+import messages_pb2
+import json
+
+STORAGE_NODES_NUM = 4
+
+# TO BE DONE: placeholder for store_file
+
+# TO BE DONE: placeholder for __decode_file
+
+# TO BE DONE: placeholder for recode
+
+# TO BE DONE: placeholder for get_file
+
+# TO BE DONE: placeholder for __store_repair_fragments
+
+# TO BE DONE: placeholder for start_repair_process
